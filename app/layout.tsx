37249304aa6c69eedbc8e45ac56856b0838d2833
Flavio F <flavio.filho@sada.com.br>
2025-04-29 13:56:19 -0300

    Passando SEO para layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,42 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Instituto Ser Feliz',
-  description: 'Escola infantil dedicada ao desenvolvimento de crianças no Bairro Industrial, Contagem',
-};
+  title: 'Instituto Ser Feliz | Creche Escolar',
+  description:
+    'Oferece educação infantil em tempo integral para crianças de 1 a 3 anos e 11 meses, atendendo cerca de 150 crianças e suas famílias',
+  keywords: [
+    'escola',
+    'creche',
+    'instituto',
+    'maternal',
+    'comunidade',
+    'industrial',
+    'contagem',
+    'maternal',
+  ],
+  authors: [{ name: 'Instituto Ser Feliz' }],
+  openGraph: {
+    title: 'Instituto Ser Feliz | Educação Infantil',
+    description:
+      'Oferece educação infantil em tempo integral para crianças de 1 a 3 anos e 11 meses, atendendo cerca de 150 crianças e suas famílias',
+    url: 'https://instituto-next.vercel.app/',
+    siteName: 'Instituto Ser Feliz',
+    images: [
+      {
+        url: 'https://instituto-next.vercel.app/instituto.jpeg',
+        width: 1200,
+        height: 630,
+        alt: 'Instituto Ser Feliz',
+      },
+    ],
+    type: 'website',
+    locale: 'pt_BR',
+  },
+  icons: {
+    icon: '/favicon.ico',
+  },
+  metadataBase: new URL('https://instituto-next.vercel.app'),
+}
 
 export default function RootLayout({
   children,

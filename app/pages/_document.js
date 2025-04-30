f20d63b0d5fa6da927163c8a28acf0033a225680
Flavio F <flavio.filho@sada.com.br>
2025-04-30 13:55:49 -0300

    Adding google site verification

---
+++ app/pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <meta
+            name="google-site-verification"
+            content="HM8EQ7aetV45DAINpGsAl9P4qjmv-Moue1rwS8a0BCM"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}

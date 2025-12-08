9fd9f37a573fee20933de99aa5af04f18d78ce3e
Flavio F <flavio.win@gmail.com>
2025-12-08 18:11:11 -0300

    Fix crossOrigin to camel case

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,7 +110,7 @@ export default function RootLayout({
       </Script>
 
       <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4946920103183663"
-        crossorigin="anonymous"></Script>
+        crossOrigin="anonymous"></Script>
       <body className={inter.className}>{children}</body>
     </html>
   );

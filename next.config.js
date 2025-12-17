8a840bde27160513466ff3a667d261096990205c
Flavio F <flavio.win@gmail.com>
2025-12-17 14:11:14 -0300

    Alterando cache control

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=2592000, stale-while-revalidate=2592000, stale-if-error=2592000',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;

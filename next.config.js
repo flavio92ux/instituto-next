1c6629066d4532a8798994132efa02fa91e6e44e
Flavio F <flavio.win@gmail.com>
2025-12-17 14:16:17 -0300

    Adding vercel.json

--- next.config.js
+++ next.config.js
@@ -5,19 +5,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=2592000, stale-while-revalidate=2592000, stale-if-error=2592000',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;

f667b858fa4f5e486a6fe170a6e93b81aab85b7f
Flavio F <flavio.win@gmail.com>
2025-12-17 21:19:44 -0300

    force webp

--- cloudflare-loader.ts
+++ cloudflare-loader.ts
@@ -1,5 +1,9 @@
 export default function cloudflareLoader({ src, width, quality }: { src: string, width: number, quality?: number }) {
-  const params = [`width=${width}`, `quality=${quality || 75}`, 'format=auto']
+  const params = [
+    `width=${width}`,
+    `quality=${quality || 75}`,
+    'format=webp'
+  ]
   
   // Se a imagem for externa (ex: S3), você precisa garantir que o 
   // Cloudflare Images tenha permissão para puxar de "Remote Sources"

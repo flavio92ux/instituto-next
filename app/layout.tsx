c41331e6c9edbf28f16825bb049e6b26043d0d21
Flavio F <flavio.win@gmail.com>
2025-12-30 10:21:02 -0300

    Removes unnecessary preconnect link from head
    Eliminates a redundant preconnect resource to optimize page load
    and simplify HTML head configuration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,8 +56,6 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <link rel="preconnect" href="https://instituto-serfeliz-industrial.online/_next/static/css/c11fa715fdda2186.css" />
-
         {/* Meta tag de verificação do Google */}
         <meta
           name="google-site-verification"

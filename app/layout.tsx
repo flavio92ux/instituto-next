57559f656238c3db9d49f54a91eaef7297879494
Flavio F <flavio.win@gmail.com>
2026-01-14 16:55:17 -0300

    Adds multi-size favicons and updates head links

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,12 +56,12 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.ico" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.ico" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.ico" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.ico" />
 
         {/* Meta tag de verificação do Google */}
         <meta
